a5aa481806ccca5d46db66a60440b4304fba5e75
Fabio Ferreira <fabio.ferreira3@outlook.com>
2017-06-29 15:26:25 -0300

    associating cart items with a Model

--- src/Darryldecode/Cart/CartCollection.php
+++ src/Darryldecode/Cart/CartCollection.php
@@ -4,4 +4,16 @@ use Illuminate\Support\Collection;
 
 class CartCollection extends Collection {
 
+    public function associate($model, $field = 'id'){
+
+        $associated = new $model;
+
+        $this->transform(function($item, $key) use ($associated, $field) {
+            $item['association'] = $associated->where($field,$key)->first();
+            return $item;
+        });
+
+        return $this;
+    }
+
 }
